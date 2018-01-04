42e44eba078efc84b38190608efee7f3a10ef25a
LAPTOP-H3QJ723G\M.Haseeb Ghafoor <haseeb.ghafoor@gmail.com>
2018-01-04 21:42:14 +0500

    header code

---
+++ head.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="headerstyle.css">
+<meta charset="utf-8">
+<title>head</title>
+</head>
+
+<body>
+<table border="1"  class="head">
+<tr>
+<td class="logo">
+<img alt="Loading error" src="images/logo.jpg"/>
+</td>
+<td class="a">
+<ul>
+<li>
+<a href="#">Home</a></li>
+<li><a href="#">About us</a></li>
+<li><a href="#">Servise</a></li>
+<li><a href="#">Portfolio</a></li>
+<li><a href="#">Blog</a>
+</li></ul>
+</td>
+</tr>
+</table>
+</body>
+</html>
